89df95ef4a2d76c3e0400718f128e6dc31ae38d5
Kartik Prabhu <kartikprabhu00@gmail.com>
2017-04-29 18:06:55 -0400

    add JSON storage to create and update; read from JSON directly; removed mf parsing

--- functions.py
+++ functions.py
@@ -1,13 +1,14 @@
 import os
 import codecs
+import json
 import tempfile
 
 import jinja2
 import jinja2.ext
 
-import mf2py
-
 import filters
+
+SUBDIR = 'notes'
 
 def _ensure_dir(f):
 	d = os.path.dirname(f)
@@ -35,40 +36,49 @@ def create(storage, data):
 		uid = data['properties']['uid'][0]
 		uid = uid.strip()
 
-		if not uid:
+		if not uid: 
 			return {'code': 400, 'message': 'uid of data not valid'}
 	except KeyError:
 		return {'code': 400, 'message': 'uid of data not found'}
 		pass
 
-	path = os.path.join(storage, 'notes', uid, uid+'.html')
-
-	# make sure the directories exist
-	_ensure_dir(path)
+	dir_path = os.path.join(storage, SUBDIR, uid)
+	html_path = os.path.join(dir_path, uid+'.html')
 
 	#  if file already exists return error
-	if os.path.exists(path):
+	if os.path.exists(html_path):
 		return {'code': 409, 'message': 'File already exists'}
 
-	# create a file;
-	with codecs.open(path, 'w', 'utf-8') as f:
+	# make sure the directories exist
+	_ensure_dir(html_path)
+
+	# create HTML file
+	with codecs.open(html_path, 'w', 'utf-8') as f:
 		f.write(_render(data['properties']))
+
+	json_path = os.path.join(dir_path, uid+'.json')
+
+	# create JSON file for data
+	with codecs.open(json_path, 'w', 'utf-8') as f:
+		json.dump(data, f, ensure_ascii=False)
 
 	return {'code': 200, 'message': 'File stored'}
 
 def read(storage, uid):
 
-	path = os.path.join(storage, 'notes', uid, uid+'.html')
+	# read directly from json; create function should make JSON
 
-	if os.path.exists(path):
+	dir_path = os.path.join(storage, SUBDIR, uid)
+	json_path = os.path.join(dir_path, uid+'.json')
 
-		with codecs.open(path, 'r', 'utf-8') as f:
-			data = mf2py.Parser(doc=f).to_dict(filter_by_type='h-entry')
-		# only return the first h-entry
-		return {'code': 200, 'message': 'File read', 'data': data[0]}
-
-	else:
+	# if JSON exists read from JSON
+	if not os.path.exists(json_path):
 		return {'code': 404, 'message': 'File not found'}
+
+	with codecs.open(json_path, 'r', 'utf-8') as f:
+		data = json.load(f)
+
+	return {'code': 200, 'message': 'File read', 'data': data}
 
 def update(storage, data):
 
@@ -84,34 +94,36 @@ def update(storage, data):
 		pass
 
 	# open file. if does not exist throw error
-	dir_path = os.path.join(storage, 'notes', uid)
-	path = os.path.join(dir_path, uid+'.html')
+	dir_path = os.path.join(storage, SUBDIR, uid)
+	json_path = os.path.join(dir_path, uid+'.json')
 
-	if os.path.exists(path):
-		old_data = None
-
-		with codecs.open(path, 'r', 'utf-8') as f:
-			old_data = mf2py.Parser(doc=f).to_dict(filter_by_type='h-entry')
-
-		# don't allow uid change through update
-		if old_data['properties']['uid'][0] == uid:
-
-			# create temp file to write data
-			with tempfile.NamedTemporaryFile(delete=False, dir=dir_path) as temp_f:
-				temp_f.write(_render(data['properties']).encode('utf-8'))
-				# replace original file
-				os.rename(temp_f.name, path)
-
-			return {'code': 200, 'message': 'File updated'}
-
-		else:
-			return {'code': 400, 'message': 'uid of data does not match uid of file'}
-
-	else:
+	if not os.path.exists(json_path):
 		return {'code': 404, 'message': 'File not found'}
 
-	# return status of update
-	return None
+	old_data = None
+
+	with codecs.open(jsonpath, 'r', 'utf-8') as f:
+		old_data = json.load(f)
+
+	# don't allow uid change through update
+	if old_data['properties']['uid'][0] != uid:
+		return {'code': 400, 'message': 'uid of data does not match uid of file'}
+
+	# create temp file to write json data
+	with tempfile.NamedTemporaryFile(delete=False, dir=dir_path) as temp_f:
+		json.dump(data, temp_f, ensure_ascii=False)
+		# replace original json file
+		os.rename(temp_f.name, json_path)
+
+	html_path = os.path.join(dir_path, uid+'.html')
+
+	# create temp file to write html data
+	with tempfile.NamedTemporaryFile(delete=False, dir=dir_path) as temp_f:
+		temp_f.write(_render(data['properties']).encode('utf-8'))
+		# replace original file
+		os.rename(temp_f.name, html_path)
+
+	return {'code': 200, 'message': 'File updated'}
 
 def extend(storage, uid, data):
 
