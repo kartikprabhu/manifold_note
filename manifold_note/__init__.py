dca990d3384300c8dd902f43674c80f0bae04b07
Kartik Prabhu <kartikprabhu00@gmail.com>
2017-04-30 15:38:05 -0400

    add init file to import functions

---
+++ manifold_note/__init__.py
@@ -0,0 +1,9 @@
+#/usr/bin/env python
+
+__author__    = 'Kartik Prabhu'
+__contact__   = 'me@kartikprabhu.com'
+__copyright__ = 'Copyright (c) by Kartik Prabhu'
+__license__   = 'MIT'
+__version__   = '0.1.0'
+
+from functions import create, read, update, delete, undelete
